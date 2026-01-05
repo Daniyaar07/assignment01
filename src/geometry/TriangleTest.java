5d9db354943f7b5ced13fdc9435aad08f1ec79f9
Данияр <daniyarbaikanov07@gmail.com>
2026-01-05 22:32:27 +0500

    ghfhgfgds

--- src/geometry/TriangleTest.java
+++ src/geometry/TriangleTest.java
@@ -1,12 +1,11 @@
 package geometry;
 
-import org.w3c.dom.ls.LSOutput;
-
 public class TriangleTest {
     public static void main (String[] args) {
-        Triangle t1 = new Triangle(
-                new Point(0, 0), new Point(4, 0), new Point(0, 3)
-        );
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(4, 0);
+        Point p3 = new Point(0, 3);
+        Triangle t1 = new Triangle(p1, p2, p3);
         Point p = new Point(1, 1);
         System.out.println("Area:" + t1.getArea());
         System.out.println("Perimeter:" + t1.getPerimeter());
