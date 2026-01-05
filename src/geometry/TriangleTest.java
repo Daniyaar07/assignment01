e5304693fc0f6992624341a4e6540cdd48134b92
Данияр <daniyarbaikanov07@gmail.com>
2026-01-05 22:35:12 +0500

    ghfhgfgds

--- src/geometry/TriangleTest.java
+++ src/geometry/TriangleTest.java
@@ -4,7 +4,7 @@ public class TriangleTest {
     public static void main (String[] args) {
         Point p1 = new Point(0, 0);
         Point p2 = new Point(4, 0);
-        Point p3 = new Point(0, 3);
+        Point p3 = new Point(0, 2);
         Triangle t1 = new Triangle(p1, p2, p3);
         Point p = new Point(1, 1);
         System.out.println("Area:" + t1.getArea());
